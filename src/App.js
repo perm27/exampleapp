5fb655bbd243bda735d7409cb38b2e67560ce61d
perm <37287524+perm27@users.noreply.github.com>
2025-08-27 14:39:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <h1>Hello from V2</h1>
-        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
