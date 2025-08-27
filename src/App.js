330ebe592616387a0226971cd769e9d30901e882
Hiroaki NAGAMATSU <hnagamatsu@solxyz.co.jp>
2025-08-27 14:37:10 +0900

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <h1>Hello from V2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
